586d9f77e156c280d83b26d34be93552e466572e
deeninsom <syihab855@gmail.com>
2024-01-02 20:58:04 +0700

    add seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+import * as bcrypt from 'bcryptjs';
+
+async function main() {
+  const hashedPassword = await bcrypt.hash('admin123', 10);
+
+  const user = await prisma.user.upsert({
+    create: {
+      email: 'admin@gmail.com',
+      username: 'admin',
+      password: hashedPassword
+    },
+    where: { email: 'admin@gmail.com' },
+    update: {}
+  })
+
+  await prisma.post.upsert({
+    create: {
+      caption: 'lorem dipsum',
+      tags: 'lorem',
+      likes: 0,
+      author_id: user.id
+    },
+    where: {
+      id: ''
+    },
+    update: {}
+  })
+
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
